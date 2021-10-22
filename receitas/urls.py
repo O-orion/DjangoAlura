645d7de5c4a750a11012408a2fb52c77b54e73cb
Lucas Viana <lucas.lvtorres@gmail.com>
2021-10-22 10:27:18 -0400

    Jesus Cristo é o Senhor - add rota amigo

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path #importando as urls do django
 from . import views #importando todas as views, views manipula qual urls queremos mostra
 
 urlpatterns = [
-    path('',views.index, name="index") #primeiro parãmetro e a rota, a segunda ea view responsavel por essa rota, namespace para as entradas urls
+    path('',views.index, name="index"), #primeiro parãmetro e a rota, a segunda ea view responsavel por essa rota, namespace para as entradas urls
+    path('amigo', views.meuHeroi, name='amigo')
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>JESUS CRISTO É O SENHOR!</h1><br> <h2>Obrigado por tudo!</h2>')
+
+def meuHeroi(request):
+    return HttpResponse('<h1>Meu melhor amigo</h1><br><p>sempre será você Senhor Jesus Cristo! </p>')
